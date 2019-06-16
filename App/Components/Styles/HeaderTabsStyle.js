1a96b486a3e12b487268525daa81118a70d92aab
nderitunick <nderitunick@gmail.com>
2019-06-16 11:12:06 +0300

    UI package install

---
+++ App/Components/Styles/HeaderTabsStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  row:{
+    flexDirection:'row',
+    alignItems:'center',
+    marginTop:21,
+    marginLeft:21,
+  },
+  title:{
+    fontFamily: "Helvetica",
+    fontSize: 16,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#303030",
+    marginLeft:22.5
+  }
+})
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,22 +1,33 @@
-import { StackNavigator } from 'react-navigation'
+import { StackNavigator ,DrawerNavigator } from 'react-navigation'
+import AddBusinessScreen from '../Containers/AddBusinessScreen'
 import HomeScreen from '../Containers/HomeScreen'
 import OnboardingScreen from '../Containers/OnboardingScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
+
+const MyDrawerNavigator = DrawerNavigator ({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  AddBusinessScreen: { screen: AddBusinessScreen },
   HomeScreen: { screen: HomeScreen },
   OnboardingScreen: { screen: OnboardingScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'HomeScreen',
+  initialRouteName: 'AddBusinessScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
 })
+
+
 
 export default PrimaryNav
