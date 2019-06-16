55ee90b04dde9346f324179fe0b3ffef205e7c31
stella <stellavinmaris@gmail.com>
2019-06-16 17:05:05 +0300

    merged

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -39,7 +39,7 @@ const PrimaryNav = StackNavigator({
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'PreviewScreen',
+  initialRouteName: 'AddBusinessScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
